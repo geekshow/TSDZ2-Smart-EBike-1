ba6e94899f2d58f4b89c762b2a62b2d35356f587
IArchi <92300377+IArchi@users.noreply.github.com>
2022-11-30 10:04:35 +0100

    Update compile_and_flash_20.sh
    Update bash script to allow later edit :)

--- compile_and_flash_20.sh
+++ compile_and_flash_20.sh
@@ -23,25 +23,40 @@ echo $release_folder/TSDZ2-$version-$settings_date.hex
 mkdir -p $release_folder
 yes | cp -rf ../../bin/main.ihx $release_folder/TSDZ2-$version-$settings_date.hex
 
+backup=no
 while true; do
-read -p "Do you want to flash the motor ? [Y/n]" yn
-case $yn in
-	y ) break;;
-	n ) exit;;
-	* ) break;;
+	read -p "Do you want to backup the firmware ? [y/N]" yn
+	case $yn in
+		y ) backup=yes; break;;
+		n ) break;;
+		* ) break;;
 esac
 done
 
 # Backup firmware
-echo Backup current firmware
-mkdir -p $backup_folder
-make backup
-yes | cp -rf ../../bin/TSDZ_orig_opt.bin $backup_folder/TSDZ_orig_opt-$version-$settings_date.bin
-yes | cp -rf ../../bin/TSDZ_orig.bin $backup_folder/TSDZ_orig-$version-$settings_date.bin
-yes | cp -rf ../../bin/TSDZ_orig_eeprom.bin $backup_folder/TSDZ_orig_eeprom-$version-$settings_date.bin
+if [ "$backup" = "yes" ]; then
+	echo Backup current firmware to $backup_folder/TSDZ_orig_opt-$version-$settings_date.bin
+	mkdir -p $backup_folder
+	make backup
+	yes | cp -rf ../../bin/TSDZ_orig_opt.bin $backup_folder/TSDZ_orig_opt-$version-$settings_date.bin
+	yes | cp -rf ../../bin/TSDZ_orig.bin $backup_folder/TSDZ_orig-$version-$settings_date.bin
+	yes | cp -rf ../../bin/TSDZ_orig_eeprom.bin $backup_folder/TSDZ_orig_eeprom-$version-$settings_date.bin
+fi
+
+flash=yes
+while true; do
+read -p "Do you want to flash the motor ? [Y/n]" yn
+case $yn in
+	y ) break;;
+	n ) flash=no; break;;
+	* ) break;;
+esac
+done
 
 # Flash new firmware
-make clear_eeprom
-make flash
+if [ "$flash" = "yes" ]; then
+	make clear_eeprom
+	make flash
+fi
 
 echo All done !
